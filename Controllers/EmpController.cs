95592dda47bd993f5ce675981c360f408c2956ae
Pankaj Kumar Maurya <pankajmaurya872691@gmail.com>
2022-12-01 13:56:33 +0530

    Add project files.

---
+++ Controllers/EmpController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using apiconsume17722.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace apiconsume17722.Controllers
+{
+    public class EmpController : Controller
+    {
+        // GET: Emp
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Employee emp)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44346/api/Reg");
+            string data = JsonConvert.SerializeObject(emp);
+            StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+            client.PostAsync(client.BaseAddress,content);
+            return View();
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace apiconsume17722.Models
+{
+    public class Employee
+    {
+        public int rid { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public Nullable<int> age { get; set; }
+        public Nullable<int> salary { get; set; }
+    }
+}
